1e66b7e5ac1381afd012835aaf8b11856a3ac577
edmond fagbuyi <edmondfagbuyi@gmail.com>
2023-07-29 17:35:08 +0100

    specifying the router

---
+++ spaceship-ui/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {BrowserRouter as Router, Routes, Route } from "react-roter-dom"
+
+function App() {
+  return (
+    <Router>
+    <Routes>
+    <Route></Route>
+    </Routes>
+    </Router>
+  );
+}
+
+export default App;
